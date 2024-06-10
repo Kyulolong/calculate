1ca032a5afe6cf986284847caa9710995b11a4a7
kyurorong <zzikcomputer@gmail.com>
2024-06-10 11:28:29 +0900

    레벨3 계산기 완성

--- final calculator.playground/Contents.swift
+++ final calculator.playground/Contents.swift
@@ -1,37 +1,4 @@
-
 //level 1, 2
-
-class Calculator {
-    var firstNumber : Double
-    var secondNumber : Double
-    
-    init(_ firstNumber: Double, _ secondNumber: Double) {
-        self.firstNumber = firstNumber
-        self.secondNumber = secondNumber
-    }
-    
-    func calculate(_ operatorchar: String, _ firstNumber: Double, _ secondNumber: Double ) -> Double {
-        if operatorchar == "+" {
-            return self.firstNumber + self.secondNumber
-      } else if operatorchar == "-" {
-          return firstNumber - secondNumber
-      } else if operatorchar == "*" {
-          return firstNumber * secondNumber
-      } else if operatorchar == "/" {
-          return firstNumber / secondNumber
-      } else if operatorchar == "%" {
-          return firstNumber.truncatingRemainder(dividingBy: secondNumber)
-    }
-    return 0
-  }
-}
-
-let calculator = Calculator(30, 20)
-let result = calculator.calculate("+", 10, 10)
-
-
-
-//level 1, 2(different)
 //class Calculator{
 //    let firstNumber : Double
 //    let secondNumber : Double
@@ -111,78 +78,51 @@ let result = calculator.calculate("+", 10, 10)
 //    func calculate(_ operatorchar : String) -> Double{
 //        switch operatorchar{
 //        case "+":
-//            let addOperation = AddOperation(firstNumber, secondNumber)
-//            return addOperation.add()
+//            let addOperation = AddOperation()
+//            return addOperation.add(self.firstNumber, self.secondNumber)
 //        case "-":
-//            let subtractOperation = SubtractOperation(firstNumber, secondNumber)
-//            return subtractOperation.min()
+//            let subtractOperation = SubtractOperation()
+//            return subtractOperation.min(self.firstNumber, self.secondNumber)
 //            // 덧셈 뺄셈은 let으로 보기 쉽게 바꿔주었음
 //        case "*":
-//            return MultiplyOperation(firstNumber, secondNumber).mul()
+//            let multiplyOperation = MultiplyOperation()
+//            return multiplyOperation.mul(self.firstNumber, self.secondNumber)
 //        case "/":
-//            return DivideOperation(firstNumber, secondNumber).div()
+//            let divideOperation = DivideOperation()
+//            return divideOperation.div(self.firstNumber, self.secondNumber)
 //        default:
 //            return 0
 //        }
 //    }
 //}
 //class AddOperation{
-//    var firstNumber : Double
-//    var secondNumber : Double
-//
-//    init(_ firstNumber: Double, _ secondNumber: Double) {
-//        self.firstNumber = firstNumber
-//        self.secondNumber = secondNumber
-//    }
-//        func add() -> Double {
+//    func add(_ firstNumber: Double, _ secondNumber: Double ) -> Double {
 //            return firstNumber + secondNumber
 //        }
 //    }
 //class SubtractOperation{
-//    var firstNumber : Double
-//    var secondNumber : Double
-//    
-//    init(_ firstNumber: Double, _ secondNumber: Double) {
-//        self.firstNumber = firstNumber
-//        self.secondNumber = secondNumber
-//    }
-//        func min() -> Double {
+//        func min(_ firstNumber: Double, _ secondNumber: Double) -> Double {
 //            return firstNumber - secondNumber
 //        }
 //    }
 //    class MultiplyOperation{
-//        var firstNumber : Double
-//        var secondNumber : Double
-//        
-//        init(_ firstNumber: Double, _ secondNumber: Double) {
-//            self.firstNumber = firstNumber
-//            self.secondNumber = secondNumber
-//        }
-//        func mul() -> Double {
+//
+//        func mul(_ firstNumber: Double, _ secondNumber: Double) -> Double {
 //            return firstNumber * secondNumber
 //    }
 //}
 //    class DivideOperation{
-//        var firstNumber : Double
-//        var secondNumber : Double
-//        
-//        init(_ firstNumber: Double, _ secondNumber: Double) {
-//            self.firstNumber = firstNumber
-//            self.secondNumber = secondNumber
-//        }
-//
-//    func div() -> Double {
+//        func div(_ firstNumber: Double, _ secondNumber: Double) -> Double {
 //        if secondNumber != 0 {
 //            return firstNumber / secondNumber
 //        } else {
 //            print("분모가 0이므로 나눌 수 없습니다")
-//            return 0 //nil 은 왜 안될까요?
+//            return 0
 //        }
 //    }
 //}
 //    let calculator = Calculator(4, 0)
 //    let result = calculator.calculate("+")
-//
 //
 //
 //    let addResult = calculator.calculate("+")
@@ -194,8 +134,81 @@ let result = calculator.calculate("+", 10, 10)
 //    print("subtractResult : \(subtractResult)")
 //    print("multiplyResult : \(multiplyResult)")
 //    print("divideResult : \(divideResult)")
-//
-//
+
+
 ////level 4
-//
-////wa
+
+class AbstractOperation {
+    func calculate(_ operationchar : String) -> Double{
+        switch operationchar{
+        case "+":
+            let addOperation = AddOperation()
+            return addOperation.add()
+        case "-":
+            let subtractOperation = SubtractOperation()
+            return subtractOperation.min(self.firstNumber, self.secondNumber)
+            // 덧셈 뺄셈은 let으로 보기 쉽게 바꿔주었음
+        case "*":
+            let multiplyOperation = MultiplyOperation()
+            return multiplyOperation.mul(self.firstNumber, self.secondNumber)
+        case "/":
+            let divideOperation = DivideOperation()
+            return divideOperation.div(self.firstNumber, self.secondNumber)
+        default:
+            return 0
+        }
+    }
+}
+
+class Calculator {
+var firstNumber : Double
+var secondNumber : Double
+var operationchar : String
+
+    init(_ firstNumber: Double, _ secondNumber: Double, operationchar: String) {
+    self.firstNumber = firstNumber
+    self.secondNumber = secondNumber
+    self.operationchar = operationchar
+}
+}
+
+class AddOperation: AbstractOperation{
+    override
+    func add(_ firstNumber: Double, _ secondNumber: Double ) -> Double {
+        return firstNumber + secondNumber
+    }
+}
+class SubtractOperation{
+    func min(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+        return firstNumber - secondNumber
+    }
+}
+class MultiplyOperation{
+
+    func mul(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+        return firstNumber * secondNumber
+}
+}
+class DivideOperation{
+    func div(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+    if secondNumber != 0 {
+        return firstNumber / secondNumber
+    } else {
+        print("분모가 0이므로 나눌 수 없습니다")
+        return 0
+    }
+}
+}
+let calculator = Calculator(4, 0)
+let result = calculator.calculate("/")
+
+
+let addResult = calculator.calculate("+")
+let subtractResult = calculator.calculate("-")
+let multiplyResult = calculator.calculate("*")
+let divideResult = calculator.calculate("/")
+
+print("addResult : \(addResult)")
+print("subtractResult : \(subtractResult)")
+print("multiplyResult : \(multiplyResult)")
+print("divideResult : \(divideResult)")
